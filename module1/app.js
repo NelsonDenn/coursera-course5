7b81ded193386e2be7dfc8d8859929fe1e9bddac
Nelson Denn <nelson.denn@outlook.com>
2019-07-29 19:12:02 -0400

    module1 assignment initial commit

---
+++ module1/app.js
@@ -0,0 +1,45 @@
+(function() {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+
+function LunchCheckController ($scope) {
+  $scope.lunchList = "";
+  $scope.message = "";
+  $scope.color = "";
+
+  $scope.checkLunchList = function () {
+    
+    // Validate that data was entered
+    if ($scope.lunchList == "") {
+      $scope.message = "Please enter data first";
+      $scope.color = "red";
+    } else {
+      $scope.color = "green";
+      var lunchItems = $scope.lunchList.split(",");
+      var lunchItemCount = 0;
+      
+      // Count lunch items, excluding empty
+      for (var i = 0; i < lunchItems.length; i++) {
+        var lunchItem = lunchItems[i];
+
+        if (lunchItem.trim() != "") {
+          lunchItemCount++;
+        }
+      }
+
+      // Display response mesage
+      if (lunchItemCount > 3) {
+        $scope.message = "Too much!";
+      } else {
+        $scope.message = "Enjoy!";
+      }
+    }
+  };
+
+};
+
+})();
